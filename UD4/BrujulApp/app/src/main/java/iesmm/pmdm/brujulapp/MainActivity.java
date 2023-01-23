b9fd734a37f044588b3ad2dc4c72f86c24a5e2ef
Gabriel-lab-rgb <grivote615@iesmartinezm.es>
2023-01-23 14:24:08 +0100

    Commit

--- UD4/BrujulApp/app/src/main/java/iesmm/pmdm/brujulapp/MainActivity.java
+++ UD4/BrujulApp/app/src/main/java/iesmm/pmdm/brujulapp/MainActivity.java
@@ -1,65 +1,114 @@
 package iesmm.pmdm.brujulapp;
 
-import static java.lang.Math.sqrt;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
 
-    private TextView text;
-   // private TextView direct;
+    private TextView orientacion;
+    private TextView punto;
+
+    private ImageView image;
+    // private TextView direct;
     private SensorManager sensorManager;
-    private Sensor giroscopio;
+    private Sensor sensorMagnetico;
 
-
+    private Sensor acelerometro;
+    private final String LOGTAG = "PMDM";
+    float[] mGravity = new float[3];
+    float[] mGeomagnetic = new float[3];
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image = this.findViewById(R.id.imageView);
 
-        text=(TextView)this.findViewById(R.id.text);
+        orientacion = (TextView) this.findViewById(R.id.Orientation);
+        punto = (TextView) this.findViewById(R.id.punto);
         sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
 
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
-            giroscopio = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-           // Log.i(LOGTAG, "Sensor acelerometro inicializado");
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+            sensorMagnetico = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+            // Log.i(LOGTAG, "Sensor acelerometro inicializado");
+        } else {
+            Toast.makeText(this, "El sensor magnetico no esta disponible ", Toast.LENGTH_SHORT);
         }
-        else {
-            Toast.makeText(this, "El sensor de giroscopio no esta disponible", Toast.LENGTH_SHORT);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+            acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            // Log.i(LOGTAG, "Sensor acelerometro inicializado");
+        } else {
+            Toast.makeText(this, "El sensor acelerometro no esta disponible ", Toast.LENGTH_SHORT);
         }
-        sensorManager.registerListener(this, giroscopio, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorMagnetico, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mGravity, 0, 3);
 
-                // Axis of the rotation sample, not normalized yet.
-                float axisX = event.values[0];
-                float axisY = event.values[1];
-                float axisZ = event.values[2];
+        } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mGeomagnetic, 0, 3);
 
-                // Calculate the angular speed of the sample
-                float omegaMagnitude = (float) sqrt(axisX * axisX + axisY * axisY + axisZ * axisZ);
-                text.setText(String.valueOf(omegaMagnitude));
+        }
+        if (mGravity != null && mGeomagnetic != null) {
+            float[] R = new float[9];
+            float[] I = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float[] orientation = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                float azimut = orientation[0]; // orientation contains: azimut, pitch and roll
+                float azimuthInDegress = (float) (Math.toDegrees(azimut) + 360) % 360;
+                orientacion.setText((int) azimuthInDegress + "º");
 
+                if ((int) azimuthInDegress == 360) {
+                    punto.setText("N");
+                } else if ((int) azimuthInDegress < 360 && (int) azimuthInDegress > 270) {
+                    punto.setText("NOESTE");
+                } else if ((int) azimuthInDegress == 270) {
+                    punto.setText("OESTE");
+                } else if ((int) azimuthInDegress < 270 && (int) azimuthInDegress > 180) {
+                    punto.setText("SOESTE");
+                } else if ((int) azimuthInDegress == 180) {
+                    punto.setText("S");
+                } else if ((int) azimuthInDegress < 180 && (int) azimuthInDegress > 90) {
+                    punto.setText("SE");
+                } else if ((int) azimuthInDegress == 90) {
+                    punto.setText("E");
+                } else if ((int) azimuthInDegress > 0 && (int) azimuthInDegress < 90) {
+                    punto.setText("NE");
+                }
+                image.animate().rotation((int) azimuthInDegress).setDuration(100).start();
+            }
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorMagnetico, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
